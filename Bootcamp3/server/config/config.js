3c9115e7ba22c47febf9a2c40baafbaab5456b7e
wen-yu-chen <wenyuchen@ufl.edu>
2019-09-30 22:55:22 -0400

    first commit

---
+++ Bootcamp3/server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+      uri: 'mongodb+srv://guest:2020Suwift.js@bootcamp-c8yzt.mongodb.net/test?retryWrites=true&w=majority',//place the URI of your mongo database here.
+    }, 
+    openCage: {
+      key: '2e17cf4761e64203a6ede8968a17c27d' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+    },
+    port: 8080
+  };
